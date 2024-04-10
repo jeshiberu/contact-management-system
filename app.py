fbb4c6544a7f39d8a377fa447e5c533a3abc6668
jeshiberu <166085392+jeshiberu@users.noreply.github.com>
2024-04-10 22:46:32 +0000

    added requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def Contact_Management_System():
-  return "<p>Contact Management System</p>"
+  return render_template('home.html')
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', port=8080, debug=True)
   
