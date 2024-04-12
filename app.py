831cc594aa09f2ba9881fe795beb0d83627e975d
jeshiberu <166085392+jeshiberu@users.noreply.github.com>
2024-04-12 00:14:51 +0000

    added connection to db

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+from database import load_Contacts_from_db
+
 app = Flask(__name__)
 
+ 
 @app.route('/')
 def Contact_Management_System():
-  return render_template('home.html')
+  Contact = load_Contacts_from_db()
+  return render_template('home.html', Contacts=Contacts)
 
-if __name__ == "__main__":
+@app.route("/api/contacts")
+def list_contacts():
+  return jsonify(CONTACTS)
+
+if __name__ == "__maicn__":
   app.run(host='0.0.0.0', port=8080, debug=True)
   
---
+++ database.py
@@ -0,0 +1,37 @@
+#import sqlalchemy
+
+# The error may be in the SQL query or connection setup
+from sqlalchemy import create_engine, text
+import os
+
+#print(sqlalchemy.__version__)
+
+#Hide DB CONNECTION DETAILS
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+
+def load_Contacts_from_db():
+  #sql connection
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from Contacts"))
+  #row
+  Contacts = []
+  for row in result.all():
+    Contacts.append(row._asdict())
+    return Contacts
+
+
+  # print("type(result):", type(result))
+  # print("\n")
+  # result_all = result.all()
+  # print(result_all)
+  # print("\n")
+  # print("type(result.all())", type(result_all)) 
+  # print("\n")
+  # first_result = result_all[0]
+  # print("type(first_result):", type(first_result)) 
+  # print("\n")
+  # first_result_dict = result_all[0]._asdict()
+  # print("type(first_result_dict):", type(first_result_dict))
+  # print(first_result_dict)
+
