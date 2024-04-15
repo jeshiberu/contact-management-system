706d0eb1d160297fd1c543f8768795b34597d066
jeshiberu <166085392+jeshiberu@users.noreply.github.com>
2024-04-15 07:04:47 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template
-from database import load_Contacts_from_db
+from flask import Flask, jsonify, render_template
+from database import load_Contacts_from_db, load_Contact_from_db
 
 app = Flask(__name__)
 
  
 @app.route('/')
 def Contact_Management_System():
-  Contact = load_Contacts_from_db()
-  return render_template('home.html', Contacts=Contacts)
+  Contacts = load_Contacts_from_db()
+  return render_template('home.html', 
+                         Contacts=Contacts,
+                         Contact_Management_System='Contact Management System')
 
 @app.route("/api/contacts")
 def list_contacts():
   return jsonify(CONTACTS)
 
-if __name__ == "__maicn__":
+
+@app.route("/contact/<id>")
+def show_contact(id):
+  contact = load_Contacts_from_db(id)
+  return jsonify(contact)
+
+
+
+if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
   
--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 #import sqlalchemy
-
 # The error may be in the SQL query or connection setup
 from sqlalchemy import create_engine, text
 import os
@@ -9,18 +8,33 @@ db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 #print(sqlalchemy.__version__)
 
-
+engine = create_engine(db_connection_string)
 
 def load_Contacts_from_db():
   #sql connection
   with engine.connect() as conn:
     result = conn.execute(text("select * from Contacts"))
+  print(result.all())
+
+  
   #row
   Contacts = []
   for row in result.all():
     Contacts.append(row._asdict())
     return Contacts
 
+
+def load_Contact_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(
+      "SELECT * FROM Contacts WHERE id = :val"), val=id
+    )
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
+      
 
   # print("type(result):", type(result))
   # print("\n")
